8b667e8eb65b72772b173f7d8ad45dc2534f291d
Varun Mohan <varun.mohan@ovhcloud.com>
2022-07-13 00:24:03 +0530

    main code for vm provision

---
+++ main.tf
@@ -0,0 +1,37 @@
+data "vsphere_datacenter" "dc" {
+  name = "pcc-149-202-254-29_datacenter2863-AG"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "pcc-005268"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = "Cluster1/Varun"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = "public"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = "terraform-test"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = 2
+  memory   = 1024
+  guest_id = "other3xLinux64Guest"
+
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+}
